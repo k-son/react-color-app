99c7cc341ffa25f619d214add287ed846ed5421d
Jacek Jarczok <kson.eu@gmail.com>
2020-12-04 13:27:37 +0100

    Initial upload

---
+++ src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ColorBox extends Component {
+  render() { 
+    return ( 
+      <div className="ColorBox">
+        <span>MORE</span>
+      </div>
+    );
+  }
+}
+ 
+export default ColorBox;
