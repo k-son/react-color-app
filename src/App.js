b0fa80a03407894b14a5270e4f887226560c39a5
Jacek Jarczok <kson.eu@gmail.com>
2020-12-15 15:18:13 +0100

    Add SingleColorPalette and Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import SingleColorPalette from './SingleColorPalette';
 import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
@@ -31,7 +32,7 @@ class App extends Component {
         <Route 
           exact
           path="/palette/:paletteId/:colorId"  
-          render={() => <h1>Abrakadabra</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
       //<div className="App">
