1f194a522e9cd4493e977190a534c7b108eefc35
Jacek Jarczok <kson.eu@gmail.com>
2020-12-17 16:57:56 +0100

    Add Navbar and Footer to SingleColorPalette

--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 import './Palette.css'; // must come after rc-slider styles to override them
 class Palette extends Component {
   constructor(props) {
@@ -41,14 +42,15 @@ class Palette extends Component {
           level={level} 
           changeLevel={this.changeLevel} 
           handleChange={this.changeFormat}
+          showingAllColors
         />
         <div className="Palette-colors">
           {colorBoxes}
         </div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter 
+          paletteName={paletteName}
+          emoji={emoji}
+        />
       </div>
     );
   }
